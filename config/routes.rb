604a6a3bfd3b170bfbaa3b980e44efd1f1aa2e92
umangce <kothari.umang138@gmail.com>
2021-03-05 15:40:59 +0530

    updated styles for SignIn/SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
-Rails
-  .application
-  .routes
-  .draw do
-    resources :friends
+Rails.application.routes.draw do
+  devise_for :users  
+  resources :friends
 
-    # get 'home/index'
-    get 'home/about'
-    root 'home#index'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  end
+  # get 'home/index'
+  get 'home/about'
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
 
 # rails g controller home index
 # rails g scaffold friends first_name:string last_name:string email:string phone:string twitter:string
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_120351) do
-  create_table 'friends', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
-    t.string 'phone'
-    t.string 'twitter'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_03_02_051600) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
