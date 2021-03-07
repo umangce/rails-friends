5050c929d65dc0ba5ca846f75c5197a99ea4c530
umangce <kothari.umang138@gmail.com>
2021-03-07 09:38:49 +0530

    did design changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # get 'home/index'
   get 'home/about'
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
