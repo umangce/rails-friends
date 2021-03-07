2ef2c892d8da7706cbd56edee775ac9896eeacea
umangce <kothari.umang138@gmail.com>
2021-03-07 08:28:11 +0530

    added relations to table

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy ]
 
   # GET /friends or /friends.json
   def index
@@ -11,7 +13,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -19,7 +22,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -64,6 +68,11 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: 'You are not allowed to edit this' if @friend.nil?
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
@@ -75,6 +84,6 @@ class FriendsController < ApplicationController
   def friend_params
     params
       .require(:friend)
-      .permit(:first_name, :last_name, :email, :phone, :twitter)
+      .permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
   end
 end
